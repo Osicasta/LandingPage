db1e54fc97a4998dd16d8a0361122337ec487087
Osicasta <oscar.castano@upb.edu.co>
2024-08-21 18:13:15 -0500

    organización carpetas

--- js/translate.js
+++ js/translate.js
@@ -1,9 +1,3 @@
-bars = document.querySelector(".bars");
-bars.onclick = function() {
-    navBar = document.querySelector(".nav-bar");
-    navBar.classList.toggle("active")
-}
-
 
 var polyglot = new Polyglot();
 
@@ -64,5 +58,3 @@ document.getElementById("btn-en").addEventListener("click", function () {
 document.getElementById("btn-es").addEventListener("click", function () {
     setLanguage('es');
 });
-
-
