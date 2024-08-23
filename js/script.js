2730dd4c87490df283e3b36c3970ac749356db75
Osicasta <oscar.castano@upb.edu.co>
2024-08-23 00:24:59 -0500

    arreglo imagenes click y bundle funcionando con ofuscacion de codigo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 //Abre las barras de navegación responsive
 bars = document.querySelector(".bars");
 bars.onclick = function() {
@@ -9,6 +10,11 @@ document.getElementById('logoutButton').addEventListener('click', function () {
     setTimeout(function () {
         window.location.href = 'index.html';  
     }, 1000); 
-});
+});*/
+
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('0=1.3(".0");0.5=2(){4=1.3(".6-7");4.8.9("a")}1.l(\'d\').e(\'f\',2(){g(2(){h.i.j=\'k.c\'},b)});',22,22,'bars|document|function|querySelector|navBar|onclick|nav|bar|classList|toggle|active|1000|html|logoutButton|addEventListener|click|setTimeout|window|location|href|index|getElementById'.split('|'),0,{}))
+
+
+
 
 
--- prueba.js
+++ prueba.js
@@ -1,7 +1,7 @@
 const esbuild = require('esbuild');
 
 esbuild.build({
-  entryPoints: ['/js/script.js'],  // Archivo principal de JavaScript
+  entryPoints: ['./js/script.js'],  // Archivo principal de JavaScript
   bundle: true,                // Empaqueta todos los módulos en uno solo
   minify: true,                // Minifica el código
   outfile: 'dist/apprueba.js',      // Salida del archivo empaquetado y minificado
