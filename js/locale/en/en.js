f53a5bd92d8372eb278ec05d5f3a255f2220d7bc
Osicasta <oscar.castano@upb.edu.co>
2024-08-23 01:13:46 -0500

    intento polyglot y json

---
+++ js/locale/en/en.js
@@ -0,0 +1,9 @@
+export default { 
+
+    /* Menu */
+    "besto_menu_1":"Gallery",
+    "besto_menu_2":"Form",
+    "besto_menu_3":"Video",
+    "besto_menu_4":"Contact us",
+  
+    };
---
+++ js/locale/es/es.js
@@ -0,0 +1,9 @@
+export default { 
+
+    /* Menu */
+    "besto_menu_1":"Galería",
+    "besto_menu_2":"Formulario",
+    "besto_menu_3":"Video",
+    "besto_menu_4":"Contáctanos",
+  
+    };
